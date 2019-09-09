c2e98758450506b2e8e0686dd1d4c4a66905b77d
taishiyamamoto <ambitious0816@gmail.com>
2019-09-10 02:05:11 +0900

    first commit

---
+++ src/talker.cpp
@@ -0,0 +1,46 @@
+#include<ros/ros.h>
+#include<std_msgs/Float32.h>
+
+class Talker{
+    public:
+        Talker(){
+            pub_=nh_.advertise<std_msgs::Float32>("talk_number",1000);
+        }
+    
+        void send_msgs(float number);
+
+    private:
+        ros::NodeHandle nh_;
+        ros::Publisher pub_;
+        
+        std_msgs::Float32 msg;
+};
+
+void
+Talker::send_msgs(float number){
+
+    msg.data=number;
+
+    pub_.publish(msg);
+
+}
+
+int main(int argc,char **argv){
+    ros::init(argc,argv,"talker");
+
+    Talker tk;
+
+    ros::Rate loop_rate(10);
+
+    float count=0;
+    
+    while(ros::ok()){
+        tk.send_msgs(count);
+
+        ros::spinOnce();
+        loop_rate.sleep();
+        ++count;
+    }
+
+    return 0;
+}
